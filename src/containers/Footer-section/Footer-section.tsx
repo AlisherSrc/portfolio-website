a3ace700e3622ba1d3629d25cd9e38062d6b2f69
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-05-11 20:00:29 +0600

    implement email sending functionaloty
    Downloaded emailjs package to send emailjs
    
    In the footer section, using emailjs, I've implemented sending email via
    form in that section
    
    from now on clicking on media icons will send you to the desired media
    profile

--- src/containers/Footer-section/Footer-section.tsx
+++ src/containers/Footer-section/Footer-section.tsx
@@ -1,37 +1,58 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import './footer-section.scss';
 
 import instagram_icom from "./../../assets/instagram-icon.svg";
 import twitter_icon from "./../../assets/twitter-icon.svg";
 
+import emailjs from '@emailjs/browser';
 
 const FooterSection = () => {
+    const formRef = useRef<HTMLFormElement>(null);
+    const [success,setSuccess] = useState<boolean>(false);
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        if(formRef.current){
+            emailjs.sendForm('service_zjwel1e','template_as12w9i',formRef.current,'DHlm68mwpqcJ6QzMx')
+            .then((res) => {
+                console.log(formRef.current);
+                setSuccess(true);
+                formRef.current?.reset();
+            },(error) => {
+                console.error(error.text);
+            });
+        };
+    }
+
     return (
         <section className="footer-section">
             <div className="footer__top">
                 <div className="footer__top__left">
-                    <form>
+                    <form ref={formRef} onSubmit={handleSubmit} >
                         <div className="form__userinfo">
                             <div>
                                 <p>NAME:</p>
-                                <input type="text" className="form__name" />
+                                <input type="text" name="name" className="form__name" />
                             </div>
                             <div>
                                 <p>EMAIL:</p>
-                                <input type="email" className="form__email" />
+                                <input type="email" name="email" className="form__email" />
                             </div>
                         </div>
 
                         <div className="form__message__container">
                             <p>MESSAGE:</p>
-                            <textarea rows={15} cols={75} className="form__message" />
+                            <textarea  name="message" rows={15} cols={75} className="form__message" />
                         </div>
 
                         <div className="form__buttons">
                             <input type="submit" value='SEND MESSAGE' />
-                            <input type="button" value='CLEAR' />
+                            <input type="button" value='CLEAR' onClick={() => formRef.current?.reset()} />
                         </div>
+                        {success && <p className="form__success_message">Thanks! I will reply as soon as possible</p>}
                     </form>
                 </div>
                 <div className="footer__top__right">
@@ -41,10 +62,10 @@ const FooterSection = () => {
             <div className="footer__bottom">
                 <ul>
                     <li>
-                        <img src={instagram_icom} />
+                        <a href="https://www.instagram.com/a_akito_o/" target="blank"><img src={instagram_icom} /></a>
                     </li>
                     <li>
-                        <img src={twitter_icon} />
+                        <a href="https://twitter.com/alishror?s=21&t=lv-SYWat5HQHm75cuHNWGg" target="blank"><img src={twitter_icon} /></a>
                     </li>
                 </ul>
             </div>
