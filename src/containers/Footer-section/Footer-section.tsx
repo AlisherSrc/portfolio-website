37f6a4812cccb01388b096271da08067bdf14ef2
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-05-11 16:23:10 +0600

    done with footer section
    added new color to the global CSS variables --dark-blue

--- src/containers/Footer-section/Footer-section.tsx
+++ src/containers/Footer-section/Footer-section.tsx
@@ -12,15 +12,30 @@ const FooterSection = () => {
             <div className="footer__top">
                 <div className="footer__top__left">
                     <form>
-                        <input type="text" />
-                        <input type="email" />
-                        <input type="text" />
-                        <input type="submit"/>
-                        <input type="button" />
+                        <div className="form__userinfo">
+                            <div>
+                                <p>NAME:</p>
+                                <input type="text" className="form__name" />
+                            </div>
+                            <div>
+                                <p>EMAIL:</p>
+                                <input type="email" className="form__email" />
+                            </div>
+                        </div>
+
+                        <div className="form__message__container">
+                            <p>MESSAGE:</p>
+                            <textarea rows={15} cols={75} className="form__message" />
+                        </div>
+
+                        <div className="form__buttons">
+                            <input type="submit" value='SEND MESSAGE' />
+                            <input type="button" value='CLEAR' />
+                        </div>
                     </form>
                 </div>
                 <div className="footer__top__right">
-                    <p>Let's work together to build something amazing. Contact me to get started</p>
+                    <p>Let's work together to build something amazing. Contact me to get started!</p>
                 </div>
             </div>
             <div className="footer__bottom">
