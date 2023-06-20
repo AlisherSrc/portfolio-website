1986008e71cd89153ee733550a53b16f939af19d
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-06-20 22:14:20 +0600

    continue adding hover effect to the works section

--- src/containers/Works-section/Works-section.tsx
+++ src/containers/Works-section/Works-section.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { CSSProperties, useState } from "react";
 import './works-section.scss'
 import projects from "../../data/projects";
 import Project from "../../components/project/Project";
@@ -7,30 +7,74 @@ import Project from "../../components/project/Project";
 
 
 const WorksSection = () => {
-    const [workNum,setWorkNum] = useState(0);
+    const [workNum, setWorkNum] = useState(0);
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-    const setWorkNumValidate = (id : number) =>{
+    const setWorkNumValidate = (id: number) => {
         const maxIndex = projects.length - 1;
-        (id <= maxIndex && id >= 0) && setWorkNum(id) 
+        (id <= maxIndex && id >= 0) && setWorkNum(id)
     }
 
+
+
+    const handleMouseMovement = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        const target = e.currentTarget as HTMLElement;
+        const boxElements = target.querySelectorAll(".box");
+
+        if (boxElements.length > 0) {
+            const rect = boxElements[0].getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
+            setMousePosition({x:x,y:y});
+
+        }
+    }
+
+
+    // Styles
+
+    interface boxCSSProperties extends CSSProperties{
+        "--mouse-x": string,
+        "--mouse-y": string
+    } 
+
+    const boxProperties: boxCSSProperties = {
+        "--mouse-x":`${mousePosition.x}px`,
+        "--mouse-y":`${mousePosition.y}px`
+    };
+
     return (
-        <section className="works-section">
+        <section className="works-section" onMouseMove={handleMouseMovement}>
             <div className="column column-first">
                 <div className="works-list">
                     <ul>
                         {projects.map((project) => (
                             <li key={project.id} onClick={() => setWorkNumValidate(project.id)}>
-                                {project.title}
+                                <div className="works-list__item__container box"
+                                style={boxProperties}>
+                                    <div className="box-content">
+                                        {project.title}
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </ul>
                 </div>
             </div>
+
             <div className="column column-second">
-                <div className="project-cards">
-                    <Project id={projects[workNum].id} title={projects[workNum].title} subtitle={projects[workNum].subtitle} images={projects[workNum].images}/>
+
+                <div className="project-cards box"
+                style={boxProperties}>
+                    <div className="box-content">
+                        <Project
+                            id={projects[workNum].id}
+                            title={projects[workNum].title}
+                            subtitle={projects[workNum].subtitle}
+                            images={projects[workNum].images} />
+                    </div>
                 </div>
+
             </div>
 
         </section>
